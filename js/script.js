c78867af5bc4e28d9347e744434a91c5aaae5e88
anshpatel2106 <anshpatel2106@gmail.com>
2022-11-06 08:59:26 -0800

    hiiiiiiii

---
+++ js/script.js
@@ -0,0 +1,37 @@
+ <div class="row">
+                        <div class="timeline-box padd-15">
+                            <div class="timeline  shadow-dark">
+                               <!-- timeline items -->
+                               <div class="timeline-item">
+                                <div class="circle-dot"></div>
+                                    <h3 class="timeline-date">
+                                        <i class="fa fa-calendar"></i> 2021-2022
+                                    </h3> 
+                                    <h4 class="timeline-title">PASSED 10<subscript>th</subscript> GRADE BY 88%</h4>
+                                    <p class="timeline-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, adipisci ea exercitationem modi nihil deleniti. Vel quisquam hic impedit tenetur reprehenderit eius dolore consequuntur dignissimos laudantium inventore quasi, praesentium alias.</p>
+                                
+                               </div>
+                                <!-- timeline items -->
+                                <div class="timeline-item">
+                                    <div class="circle-dot"></div>
+                                        <h3 class="timeline-date">
+                                            <i class="fa fa-calendar"></i> 2021-2022
+                                        </h3> 
+                                        <h4 class="timeline-title">PASSED 10<subscript>th</subscript> GRADE BY 88%</h4>
+                                        <p class="timeline-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, adipisci ea exercitationem modi nihil deleniti. Vel quisquam hic impedit tenetur reprehenderit eius dolore consequuntur dignissimos laudantium inventore quasi, praesentium alias.</p>
+                                    
+                                   </div>
+                                 <!-- timeline items -->
+                               <div class="timeline-item">
+                                <div class="circle-dot"></div>
+                                    <h3 class="timeline-date">
+                                        <i class="fa fa-calendar"></i> 2021-2022
+                                    </h3> 
+                                    <h4 class="timeline-title">PASSED 10<subscript>th</subscript> GRADE BY 88%</h4>
+                                    <p class="timeline-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, adipisci ea exercitationem modi nihil deleniti. Vel quisquam hic impedit tenetur reprehenderit eius dolore consequuntur dignissimos laudantium inventore quasi, praesentium alias.</p>
+                               
+                               </div>   
+                            </div>
+                             
+                            </div>
+                        </div>
